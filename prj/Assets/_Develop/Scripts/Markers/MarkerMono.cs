4aaf0f437e5eab339e115b9cb2ddfa4bea2438c6
matsu_friends <pikapikan0526@gmail.com>
2023-02-11 23:48:55 +0900

    feat: 配置可能位置を可視化するMarkerを実装

---
+++ prj/Assets/_Develop/Scripts/Markers/MarkerMono.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace ProgramDesignMock230211.Markers
+{
+    /// <summary>
+    /// 配置可能位置を可視化するMonoBehaviour
+    /// </summary>
+    public class MarkerMono : MonoBehaviour
+    {
+        /// <summary>
+        /// ワールド座標を指定する
+        /// </summary>
+        /// <param name="worldPos">ワールド座標</param>
+        public void SetWorldPos(Vector3 worldPos)
+        {
+            transform.position = worldPos;
+        }
+    }
+}
